5d994d0d251f4037b26c8b7ddabc1dba1c4a587c
Enky <enkh6at@yahoo.com>
2022-11-21 11:34:48 +0000

    core is done getting contact using id

--- src/components/ContactsView.js
+++ src/components/ContactsView.js
@@ -6,19 +6,14 @@ function ContactsView() {
   const [contact, setContact] = useState(false);
   console.log("here is view component")
   const location = useLocation();
-  
+  const { id } = useParams();
 
   //TODO: Get the contact to load from the params and fetch.
   //With useEffect, load the contact when params changes
   //and update contact state
 
-  // useEffect(() => {
-  //   fetch(`http://localhost:4000/contacts/`)
-  //     .then((data)=>{
-  //       console.log("view", data)
-  //       setContact(data)
-  //     })
-  // }, [])
+  
+
   useEffect(() => {
     if (location.state) {
       const { contact } = location.state;
@@ -34,7 +29,7 @@ function ContactsView() {
 
   return (
     <div>
-      <h2>Contact details</h2>
+      <h2>{id}-Contact details</h2>
       <h2>{contact.firstName}, {contact.lastName}</h2>
       <p>{contact.street}, {contact.city}</p>
     </div>
